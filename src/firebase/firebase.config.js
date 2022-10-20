fe5d7a215075911ef6bf58f9932609e354ad9ce7
Khaled Mahmud <khaledmahmud@Khaleds-MacBook-Air.local>
2022-10-20 20:37:06 +0600

    Authentication added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOWh2MnNPK396uEAaESCbWFy_iL-dzMAE",
+  authDomain: "auth-router-context-cf245.firebaseapp.com",
+  projectId: "auth-router-context-cf245",
+  storageBucket: "auth-router-context-cf245.appspot.com",
+  messagingSenderId: "902442378446",
+  appId: "1:902442378446:web:36ed95b09956b5c1e0dc9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
